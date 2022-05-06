1df005b23fcdd4e0f99b4f95255ed8939c4ffac9
SeungeonBaek <bsewo5959@gmail.com>
2022-05-06 21:52:39 +0900

    developing DDPG and TD3 agents

--- agents/DDPG.py
+++ agents/DDPG.py
@@ -11,8 +11,9 @@ from tensorflow.keras.layers import Dense
 from utils.replay_buffer import ExperienceMemory
 from utils.prioritized_memory_numpy import PrioritizedMemory
 
+
 class Actor(Model):
-    def __init__(self, obs_size, act_size, name="Actor"):
+    def __init__(self, action_space):
         super(Actor,self).__init__()
         self.initializer = initializers.he_normal()
         self.regularizer = regularizers.l2(l=0.005)
@@ -21,7 +22,7 @@ class Actor(Model):
         self.l2 = Dense(128, activation = 'relu', kernel_initializer=self.initializer, kernel_regularizer=self.regularizer)
         self.l3 = Dense(64, activation = 'relu', kernel_initializer=self.initializer, kernel_regularizer=self.regularizer)
         self.l4 = Dense(32, activation = 'relu', kernel_initializer=self.initializer, kernel_regularizer=self.regularizer)
-        self.mu = Dense(act_size, activation='tanh')
+        self.mu = Dense(action_space, activation='tanh')
 
     def call(self, state):
         l1 = self.l1(state)
@@ -31,6 +32,7 @@ class Actor(Model):
         mu = self.mu(l4)
 
         return mu
+
 
 class Critic(Model):
     def __init__(self):
@@ -45,7 +47,7 @@ class Critic(Model):
         self.value = Dense(1, activation = None)
 
     def call(self, state_action):
-        l1 = self.l1(state_action) # 확인
+        l1 = self.l1(state_action)
         l2 = self.l2(l1)
         l3 = self.l3(l2)
         l4 = self.l4(l3)
@@ -53,32 +55,55 @@ class Critic(Model):
 
         return value
 
+
 class Agent:
     """
-    input argument: hyper_param, obs_shape_n, act_shape_n
+    Argument:
+        agent_config: agent configuration which is realted with RL algorithm => DDPG
+            agent_config:
+                {
+                    name, gamma, tau, update_freq, use_PER, use_ERE, buffer_size, batch_size, warm_up, lr_actor_main, lr_critic_main,
+                    extension = {
+                        'gaussian_std, 'noise_reduce_rate'
+                    }
+                }
+        obs_shape_n: shpae of observation
+        act_shape_n: shape of action
 
-    hyper_param: lr_actor_main, lr_critic_main, gamma, tau, update_freq, actor_update_freq, batch_size, warm_up, gaussian_std, noise_reduce_rate
+    Methods:
+        action: return the action which is mapped with obs in policy
+        target_action: return the target action which is mapped with obs in target_policy
+        update_target: update target critic/actor network at user-specified frequency
+        update: update main critic/actor network
+        save_xp: save transition(s, a, r, s', d) in experience memory
+        load_models: load weights
+        save_models: save weights
+    
     """
-    def __init__(self, name, hyper_param, obs_shape_n, act_shape_n):
-        self.name = name
-        
-        self.actor_lr_main = hyper_param['lr_actor_main']
-        self.critic_lr_main = hyper_param['lr_critic_main']
+    def __init__(self, agent_config, obs_space, act_space):
+        self.agent_config = agent_config
+        self.name = self.agent_config['agent_name']
 
-        self.gamma = hyper_param['gamma']
-        self.tau = hyper_param['tau']
-        self.update_freq = hyper_param['update_freq']
+        self.obs_space = obs_space
+        self.act_space = act_space
+        print(f'obs_space: {self.obs_space}, act_space: {self.act_space}')
 
-        self.replay_buffer = ExperienceMemory(2000000)
-        self.batch_size = hyper_param['batch_size']
-        self.warm_up = hyper_param['warm_up']
+        self.gamma = self.agent_config['gamma']
+        self.tau = self.agent_config['tau']
+
         self.update_step = 0
-        self.std = hyper_param['gaussian_std']
-        self.reduce_rate = hyper_param['noise_reduce_rate']
+        self.update_freq = self.agent_config['update_freq']
+        
+        if self.agent_config['use_PER']:
+            self.replay_buffer = PrioritizedMemory(self.agent_config['buffer_size'])
+        else:
+            self.replay_buffer = ExperienceMemory(self.agent_config['buffer_size'])
+        self.batch_size = self.agent_config['batch_size']
+        self.warm_up = self.agent_config['warm_up']
 
-        self.obs_size = obs_shape_n
-        self.act_size = act_shape_n
-        print('obs_size: {}, act_size: {}'.format(self.obs_size, self.act_size))
+        # network config
+        self.actor_lr_main = self.agent_config['lr_actor_main']
+        self.critic_lr_main = self.agent_config['lr_critic_main']
 
         self.actor_main, self.actor_target = Actor(self.obs_size, self.act_size), Actor(self.obs_size, self.act_size)
         self.actor_target.set_weights(self.actor_main.get_weights())
@@ -90,30 +115,41 @@ class Agent:
         self.critic_opt_main = Adam(self.critic_lr_main)
         self.critic_main.compile(optimizer=self.critic_opt_main)
 
+        # extension config
+        self.extension_config = self.agent_config['extension']
+        self.std = self.agent_config['gaussian_std']
+        self.noise_clip = self.agent_config['noise_clip']
+        self.reduce_rate = self.agent_config['noise_reduce_rate']
+
     def action(self, obs):
         obs = tf.convert_to_tensor([obs], dtype=tf.float32)
-        # print('in action, obs: ', np.shape(np.array(obs)))
+        # print(f'in action, obs: {obs.shape}')
         mu = self.actor_main(obs)
-        # print('in action, mu: ', np.shape(np.array(mu)))
+        # print(f'in action, mu: {mu.shape}')
 
         if self.update_step > self.warm_up:
             std = tf.convert_to_tensor([self.std]*4, dtype=tf.float32)
             dist = tfp.distributions.Normal(loc=mu, scale=std)
             action = tf.squeeze(dist.sample())
-            self.std = self.std * self.reduce_rate
+            action = action.numpy()
+            action = np.clip(action, mu.numpy()[0]-self.noise_clip, mu.numpy()[0]+self.noise_clip)
 
-        action = mu.numpy()
-        # print('in action, action: ', np.shape(np.array(action)))
+            self.std = self.std * self.reduce_rate
+            # print(f'in action, action: {action}')
+        else:
+            action = mu.numpy()[0]
+            # print(f'in action, action: {action}')
+
         action = np.clip(action, -1, 1)
-        # print('in action, clipped_action: ', np.shape(np.array(action)))
+        # print(f'in action, clipped action: {action}')
         
         return action
 
     def target_action(self, obs):
         obs = tf.convert_to_tensor(obs, dtype=tf.float32)
-        # print('in trgt action, obs: ', np.shape(np.array(obs)))
+        # print(f'in trgt action, obs: {obs}')
         mu = self.actor_target(obs)
-        # print('in trgt action, mu: ', np.shape(np.array(mu)))
+        # print(f'in trgt action, mu: {mu}')
 
         if self.update_step > self.warm_up:
             std = tf.convert_to_tensor([self.std]*4, dtype=tf.float32)
@@ -121,9 +157,9 @@ class Agent:
             action = tf.squeeze(dist.sample())
 
         action = mu.numpy()
-        # print('in trgt action, action: ', np.shape(np.array(action)))
+        # print(f'in trgt action, action: {action}')
         action = np.clip(action, -1, 1)
-        # print('in trgt action, clipped_action: ', np.shape(np.array(action)))
+        # print(f'in trgt action, clipped_action: {action}')
 
         return action
 
@@ -142,43 +178,77 @@ class Agent:
             critic_weithgs.append(weight * self.tau + critic_targets[idx] * (1 - self.tau))
         self.critic_target.set_weights(critic_weithgs)
 
-    def update(self, steps):
+    def update(self):
         if self.replay_buffer._len() < self.batch_size:
             return False, 0.0, 0.0, 0.0, 0.0
-        if not steps % self.update_freq == 0:  # only update every update_freq
+        if not self.update_step % self.update_freq == 0:  # only update every update_freq
+            self.update_step += 1
             return False, 0.0, 0.0, 0.0, 0.0
 
         updated = True
         self.update_step += 1
         
-        states, next_states, rewards, actions, dones = self.replay_buffer.sample(self.batch_size)
-        
-        states = tf.convert_to_tensor(states, dtype = tf.float32)
-        next_states = tf.convert_to_tensor(next_states, dtype = tf.float32)
-        rewards = tf.convert_to_tensor(rewards, dtype = tf.float32)
-        actions = tf.convert_to_tensor(actions, dtype = tf.float32)
-        dones = tf.convert_to_tensor(dones, dtype = tf.bool)
+        if self.agent_config['use_PER']:
+            states, next_states, rewards, actions, dones, idxs, is_weight = self.replay_buffer.sample(self.batch_size)
+
+            if self.agent_config['reward_normalize']:
+                rewards = np.asarray(rewards)
+                rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+
+            states = tf.convert_to_tensor(states, dtype = tf.float32)
+            next_states = tf.convert_to_tensor(next_states, dtype = tf.float32)
+            rewards = tf.convert_to_tensor(rewards, dtype = tf.float32)
+            actions = tf.convert_to_tensor(actions, dtype = tf.float32)
+            dones = tf.convert_to_tensor(dones, dtype = tf.bool)
+            is_weight = tf.convert_to_tensor(is_weight, dtype=tf.float32)
+            # print('states : {}'.format(states.numpy().shape), states)
+            # print('next_states : {}'.format(next_states.numpy().shape), next_states)
+            # print('rewards : {}'.format(rewards.numpy().shape), rewards)
+            # print('actions : {}'.format(actions.numpy().shape), actions)
+            # print('dones : {}'.format(dones.numpy().shape), dones)
+            # print('is_weight : {}'.format(is_weight.numpy().shape), is_weight)
+
+        else:
+            states, next_states, rewards, actions, dones = self.replay_buffer.sample(self.batch_size)
+
+            if self.agent_config['reward_normalize']:
+                rewards = np.asarray(rewards)
+                rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+
+            states = tf.convert_to_tensor(states, dtype = tf.float32)
+            next_states = tf.convert_to_tensor(next_states, dtype = tf.float32)
+            rewards = tf.convert_to_tensor(rewards, dtype = tf.float32)
+            actions = tf.convert_to_tensor(actions, dtype = tf.float32)
+            dones = tf.convert_to_tensor(dones, dtype = tf.bool)
         
         critic_variable = self.critic_main.trainable_variables
         with tf.GradientTape() as tape_critic:
             tape_critic.watch(critic_variable)
             target_action = self.target_action(next_states)
-            
+            # print(f'target_action : {target_action.shape}')
+
             target_q_next = tf.squeeze(self.critic_target(tf.concat([next_states,target_action], 1)), 1)
+            # print(f'target_q_next : {target_q_next.shape}')
 
             target_q = rewards + self.gamma * target_q_next * (1.0 - tf.cast(dones, dtype=tf.float32))
+            # print(f'target_q : {target_q.shape}')
 
             current_q = tf.squeeze(self.critic_main(tf.concat([states,actions], 1)), 1)
+            # print(f'current_q : {current_q.shape}')
         
-            critic_loss = tf.keras.losses.MSE(target_q, current_q)
+            td_error = tf.subtract(current_q, target_q)
+            # print(f'td_error : {td_error.shape}')
+
+            critic_losses = tf.cond(tf.convert_to_tensor(self.agent_config['use_PER'], dtype=tf.bool), \
+                lambda: tf.multiply(is_weight, tf.math.square(td_error)), lambda: tf.math.square(td_error))
+            # print(f'critic_losses : {critic_losses.shape}')
+
+            critic_loss = tf.math.reduce_mean(critic_losses)
+            # print(f'critic_loss : {critic_loss.shape}')
 
         grads_critic, _ = tf.clip_by_global_norm(tape_critic.gradient(critic_loss, critic_variable), 0.5)
 
         self.critic_opt_main.apply_gradients(zip(grads_critic, critic_variable))
-
-        target_q_val = target_q.numpy()
-        current_q_val = current_q.numpy()
-        criitic_loss_val = critic_loss.numpy()
 
         actor_variable = self.actor_main.trainable_variables       
         with tf.GradientTape() as tape_actor:
@@ -191,15 +261,50 @@ class Agent:
         grads_actor, _ = tf.clip_by_global_norm(tape_actor.gradient(actor_loss, actor_variable), 0.5)
         self.actor_opt_main.apply_gradients(zip(grads_actor, actor_variable))
 
+        target_q_val = tf.math.reduce_mean(target_q).numpy()
+        current_q_val = tf.math.reduce_mean(current_q).numpy()
+        criitic_loss_val = critic_loss.numpy()
         actor_loss_val = actor_loss.numpy()
         
         self.update_target()
 
-        return updated, np.mean(criitic_loss_val), actor_loss_val, np.mean(target_q_val), np.mean(current_q_val)
+        td_error_numpy = np.abs(td_error.numpy())
+        if self.agent_config['use_PER']:
+            for i in range(self.batch_size):
+                self.replay_buffer.update(idxs[i], td_error_numpy[i])
 
-    def save_xp(self, obs, new_obs, reward, action, done):
+        return updated, actor_loss_val, criitic_loss_val, target_q_val, current_q_val
+
+    def save_xp(self, state, next_state, reward, action, done):
         # Store transition in the replay buffer.
-        self.replay_buffer.add((obs, new_obs, reward, action, done))
+        if self.agent_config['use_PER']:
+            state_tf = tf.convert_to_tensor([state], dtype = tf.float32)
+            action_tf = tf.convert_to_tensor([action], dtype = tf.float32)
+            next_state_tf = tf.convert_to_tensor([next_state], dtype = tf.float32)
+            target_action_tf = self.actor_target(next_state_tf)
+            # print(f'state_tf: {state_tf}')
+            # print(f'action_tf: {action_tf}')
+            # print(f'next_state_tf: {next_state_tf}')
+            # print(f'target_action_tf: {target_action_tf}')
+
+            target_q_next = tf.squeeze(self.critic_target(tf.concat([next_state_tf,target_action_tf], 1)), 1)
+            # print(f'target_q_next: {target_q_next}')
+            
+            current_q = tf.squeeze(self.critic_main(tf.concat([state_tf,action_tf], 1)), 1)
+            # print(f'current_q_1: {current_q_1}')
+            
+            target_q = reward + self.gamma * target_q_next * (1.0 - tf.cast(done, dtype=tf.float32))
+            # print(f'target_q: {target_q}')
+            
+            td_error = tf.subtract(target_q ,current_q)
+            # print(f'td_error: {td_error}')
+
+            td_error_numpy = np.abs(td_error)
+            # print(f'td_error_numpy: {td_error_numpy}')
+
+            self.replay_buffer.add(td_error_numpy[0], (state, next_state, reward, action, done))
+        else:
+            self.replay_buffer.add((state, next_state, reward, action, done))
 
     def load_models(self, path):
         print('Load Model Path : ', path)
